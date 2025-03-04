c03be5e1c37e922a855e2c47ae279d447d64e32a
marcmrnda <mirandamf@students.nu-lipa.edu.ph>
2025-03-04 15:28:48 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
 import Header from "./Header.jsx";
 import Form from "./Form.jsx";
+import { useEffect, useState } from "react";
+import Meme from './assets/memeimg.png'
 function App() {
+
+  const [meme, setMeme] = useState({
+    topText: "SHUT UP",
+    bottomText: "AND TAKE MY MONEY",
+    photoMeme: Meme
+  })  
+
+  const [allMeme,setAllMeme] = useState([])
+  const [memeImg, setMemeImg] = useState([])
+
+  useEffect(() => {
+    allMeme.map(el => memeImg.push(el.url))
+  })
+
+  const change = (e) => {
+    const {value,name} = e.currentTarget
+    setMeme(prev => ({
+      ...prev,
+      [name]: value.toUpperCase()
+    }))
+  }
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+    .then(response => response.json())
+    .then(dataBase => setAllMeme(dataBase.data.memes))
+  },[])
+
+  const randomPhoto = () => {
+    const rand = Math.floor(Math.random() * allMeme.length)
+    setMeme(prev => ({
+      ...prev,
+      photoMeme: memeImg[rand]
+    }))
+  }
+  
 
   return (
     <>
       <Header />
       <main className="h-screen bg-[#FFFFFF]">
-      <Form />
+      <Form change={change}
+            random={randomPhoto}
+            meme={meme}/>
       </main>
     </>
   )
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,27 +1,25 @@
-import Memes from './assets/memeimg.png'
-const Form = () => {
+
+const Form = ({meme,change,random}) => {
+        const {topText,bottomText,photoMeme} = meme
     return (
         <>
         <section className="flex flex-col justify-center items-center gap-10 mt-10 w-full">
             <article className="flex justify-evenly gap-25">
             <div className="flex flex-col justify-center w-60">
             <label htmlFor="topText" className="font-karla font-medium">Top text</label>
-            <input className="border-[.82px] border-[#D1D5DB] py-[7.37px] px-[10.64px] rounded-[4.91px] w-60" type="text" name="topText" id="topText" placeholder="meme"/>
+            <input onChange={change} className="border-[.82px] border-[#D1D5DB] py-[7.37px] px-[10.64px] rounded-[4.91px] w-60" type="text" name="topText" id="topText" value={topText} placeholder={topText}/>
             </div>
             <div className="flex flex-col justify-center w-60">
             <label htmlFor="bottomText" className="font-karla font-medium">Bottom text</label>
-            <input className="border-[.82px] border-[#D1D5DB] py-[7.37px] px-[10.64px] rounded-[4.91px] w-60" type="text" name="bottomText" id="bottomText" placeholder="mema" />
+            <input className="border-[.82px] border-[#D1D5DB] py-[7.37px] px-[10.64px] rounded-[4.91px] w-60" type="text" name="bottomText" id="bottomText" placeholder={bottomText} value={bottomText} onChange={change} />
             </div>
             </article>
-            <button type="button" className="bg-anotherViolet w-145 h-13.5 font-karla text-white font-bold align-middle rounded-lg text-lg hover:bg-purple-200 hover:text-anotherViolet cursor-pointer">Get a new meme image  &#128511;</button>
+            <button type="button" onClick={random} className="bg-anotherViolet w-145 h-13.5 font-karla text-white font-bold align-middle rounded-lg text-lg hover:bg-purple-200 hover:text-anotherViolet cursor-pointer">Get a new meme image  &#128511;</button>
             
-            <article className='flex flex-col gap-45 items-center z-10 '>
-            <img className="w-145 max-w-145" src={Memes} alt="memePhoto"/>
-            <div className='absolute flex flex-col items-center gap-50'>
-            <span className='font-impact text-white text-5xl text-outline mt-3'>SHUT UP</span>
-            <span className='font-impact text-white text-5xl text-outline '>AND TAKE MY MONEY</span>
-            </div>
-            
+            <article className='relative flex flex-col justify-center items-center '>
+            <img className="max-w-full h-auto rounded-sm" src={photoMeme} alt="memePhoto"/>
+            <span className='absolute px-[5px] font-impact text-white text-5xl text-outline top-0 mt-1'>{topText}</span>
+            <span className='absolute px-[5px] font-impact text-white text-5xl text-outline bottom-0 mb-1'>{bottomText}</span>            
             </article>
         </section>
         </>
